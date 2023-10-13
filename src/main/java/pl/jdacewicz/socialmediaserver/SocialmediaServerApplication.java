b1d9805727412c0ec0cadccbafd9c63a15dfb417
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-13 21:51:58 +0200

    Initial commit

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/SocialmediaServerApplication.java
@@ -0,0 +1,13 @@
+package pl.jdacewicz.socialmediaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jdacewicz/socialmediaserver/SocialmediaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jdacewicz.socialmediaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
