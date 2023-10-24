e4a4cbee455e1b2d14e7efb9fe1e74565d8f1a6b
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-24 20:37:01 +0200

    Implemented file name generation

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/filestorage/FileNameGenerator.java
@@ -0,0 +1,16 @@
+package pl.jdacewicz.socialmediaserver.filestorage;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+class FileNameGenerator {
+
+    static String generateFileName(String originalFileName) {
+        var extension = extractFileExtension(originalFileName);
+        return RandomStringUtils.randomAlphanumeric(16) + extension;
+    }
+
+    private static String extractFileExtension(String originalFileName) {
+        var lastDotIndex = originalFileName.lastIndexOf('.');
+        return originalFileName.substring(lastDotIndex);
+    }
+}
