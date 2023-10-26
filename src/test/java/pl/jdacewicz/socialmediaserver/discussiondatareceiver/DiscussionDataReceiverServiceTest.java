f3787e6a654535d0881a8105ebcceff1372ef99b
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-26 18:55:40 +0200

    Rebuilt DiscussionDataReceiverService tests

--- src/test/java/pl/jdacewicz/socialmediaserver/discussiondatareceiver/DiscussionDataReceiverServiceTest.java
+++ src/test/java/pl/jdacewicz/socialmediaserver/discussiondatareceiver/DiscussionDataReceiverServiceTest.java
@@ -3,15 +3,11 @@ package pl.jdacewicz.socialmediaserver.discussiondatareceiver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import pl.jdacewicz.socialmediaserver.discussiondatareceiver.dto.PostReactionRequest;
-import pl.jdacewicz.socialmediaserver.discussiondatareceiver.dto.PostRequest;
-import pl.jdacewicz.socialmediaserver.reactionuser.ReactionUserFacade;
 import pl.jdacewicz.socialmediaserver.reactionuser.dto.ReactionUser;
 import pl.jdacewicz.socialmediaserver.userdatareceiver.UserDataReceiverFacade;
 import pl.jdacewicz.socialmediaserver.userdatareceiver.dto.UserDto;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 class DiscussionDataReceiverServiceTest {
@@ -20,15 +16,13 @@ class DiscussionDataReceiverServiceTest {
 
     DiscussionDataReceiverRepositoryTest discussionDataReceiverRepository;
     UserDataReceiverFacade userDataReceiverFacade;
-    ReactionUserFacade reactionUserFacade;
 
     @BeforeEach
     void setUp() {
         userDataReceiverFacade = Mockito.mock(UserDataReceiverFacade.class);
-        reactionUserFacade = Mockito.mock(ReactionUserFacade.class);
         discussionDataReceiverRepository = new DiscussionDataReceiverRepositoryTest();
         discussionDataReceiverService = new DiscussionDataReceiverService(discussionDataReceiverRepository,
-                userDataReceiverFacade, reactionUserFacade);
+                userDataReceiverFacade);
     }
 
     @Test
@@ -58,33 +52,32 @@ class DiscussionDataReceiverServiceTest {
     @Test
     void should_return_created_post_when_creating_post() {
         //Given
-        var postRequest = new PostRequest("content");
+        var content = "content";
+        var imageName = "name";
         var loggedUserDto = UserDto.builder()
                 .build();
         when(userDataReceiverFacade.getLoggedInUser()).thenReturn(loggedUserDto);
         //When
-        var result = discussionDataReceiverService.createPost(postRequest);
+        var result = discussionDataReceiverService.createPost(content, imageName);
         //Then
-        assertEquals(postRequest.content(), result.content());
+        assertEquals(content, result.content());
+        assertEquals(imageName, result.image()
+                .fileName());
         assertEquals(loggedUserDto, result.creator());
     }
 
     @Test
-    void should_return_post_with_reaction_when_reacting_to_post() {
+    void should_return_post_with_reaction_user_when_adding_reaction_user_to_post() {
         //Given
-        var postReactionRequest = PostReactionRequest.builder()
-                .reactionId("id")
-                .postId("id")
-                .build();
+        var postId = "id";
         var reactionUser = ReactionUser.builder()
                 .build();
         var post = Post.builder()
-                .postId(postReactionRequest.postId())
+                .postId(postId)
                 .build();
-        when(reactionUserFacade.createReactionUser(any())).thenReturn(reactionUser);
         discussionDataReceiverRepository.save(post);
         //When
-        var result = discussionDataReceiverService.reactToPost(postReactionRequest);
+        var result = discussionDataReceiverService.addReactionUserToPostById(postId, reactionUser);
         //Then
         assertFalse(result.reactionUsers()
                 .isEmpty());
