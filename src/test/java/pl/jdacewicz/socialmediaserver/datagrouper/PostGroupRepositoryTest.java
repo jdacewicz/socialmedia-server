f2f7d216af1fd3f8593d8f3dd096d0016b655f28
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-11-05 15:33:01 +0100

    Written tests of PostGrouperService

---
+++ src/test/java/pl/jdacewicz/socialmediaserver/datagrouper/PostGroupRepositoryTest.java
@@ -0,0 +1,137 @@
+package pl.jdacewicz.socialmediaserver.datagrouper;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@SuppressWarnings("all")
+class PostGroupRepositoryTest implements PostGroupRepository {
+
+    List<PostGroup> database = new LinkedList<>();
+
+    @Override
+    public Optional<PostGroup> findById(String groupId) {
+        return database.stream()
+                .filter(postGroup -> postGroup.getGroupId()
+                        .equals(groupId))
+                .findFirst();
+    }
+
+    @Override
+    public <S extends PostGroup> S save(S entity) {
+        database.add(entity);
+        return entity;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public <S extends PostGroup> S insert(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends PostGroup> List<S> insert(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public <S extends PostGroup> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends PostGroup> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends PostGroup, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    @Override
+    public <S extends PostGroup> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends PostGroup> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends PostGroup> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends PostGroup> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends PostGroup> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public List<PostGroup> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<PostGroup> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(PostGroup entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends PostGroup> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<PostGroup> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<PostGroup> findAll(Pageable pageable) {
+        return null;
+    }
+}
---
+++ src/test/java/pl/jdacewicz/socialmediaserver/datagrouper/PostGrouperServiceTest.java
@@ -0,0 +1,70 @@
+package pl.jdacewicz.socialmediaserver.datagrouper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.jdacewicz.socialmediaserver.datagrouper.dto.GroupRequest;
+import pl.jdacewicz.socialmediaserver.userdatareceiver.UserDataReceiverFacade;
+import pl.jdacewicz.socialmediaserver.userdatareceiver.dto.UserDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class PostGrouperServiceTest {
+
+    PostGrouperService postGrouperService;
+
+    UserDataReceiverFacade userDataReceiverFacade;
+    PostGroupRepositoryTest postGroupRepositoryTest;
+
+    @BeforeEach
+    void setUp() {
+        userDataReceiverFacade = Mockito.mock(UserDataReceiverFacade.class);
+        postGroupRepositoryTest = new PostGroupRepositoryTest();
+        postGrouperService = new PostGrouperService(userDataReceiverFacade, postGroupRepositoryTest);
+    }
+
+    @Test
+    void should_return_post_group_when_getting_group_by_existing_id() {
+        //Given
+        var groupId = "existing id";
+        var group = PostGroup.builder()
+                .groupId(groupId)
+                .build();
+        postGroupRepositoryTest.save(group);
+        //When
+        var result = postGrouperService.getGroupById(groupId);
+        //Then
+        assertEquals(group, result);
+    }
+
+    @Test
+    void should_throw_unsupported_operation_exception_when_getting_post_by_not_existing_id() {
+        //Given
+        var groupId = "not existing id";
+        //When
+        //Then
+        assertThrows(UnsupportedOperationException.class,
+                () -> postGrouperService.getGroupById(groupId));
+    }
+
+    @Test
+    void should_return_created_group_with_owner_as_admin_and_participant_when_creating_group() {
+        //Given
+        var groupRequest = new GroupRequest("name");
+        var imageName = "imageName";
+        var loggedUser = UserDto.builder()
+                .userId("id")
+                .build();
+        when(userDataReceiverFacade.getLoggedInUser()).thenReturn(loggedUser);
+        //When
+        var result = postGrouperService.createGroup(groupRequest, imageName);
+        //Then
+        assertEquals(loggedUser.userId(), result.getOwner()
+                .userId());
+        assertFalse(result.getAdmins()
+                .isEmpty());
+        assertFalse(result.getParticipants()
+                .isEmpty());
+    }
+}
