470062bca1a3a23f843ee6fe3e2e32574ae511e3
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-11-08 18:30:17 +0100

    Moved some endpoints from BanGiver to UserDataReceiver

--- src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/controller/bangiver/BanGiverRestController.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/controller/bangiver/BanGiverRestController.java
@@ -1,12 +1,10 @@
 package pl.jdacewicz.socialmediaserver.infrastructure.controller.bangiver;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.jdacewicz.socialmediaserver.bangiver.BanGiverFacade;
-import pl.jdacewicz.socialmediaserver.bangiver.dto.PermanentBanResponse;
-import pl.jdacewicz.socialmediaserver.bangiver.dto.TemporaryBanResponse;
-import pl.jdacewicz.socialmediaserver.bangiver.dto.UserPermanentBanRequest;
-import pl.jdacewicz.socialmediaserver.bangiver.dto.UserTemporaryBanRequest;
 
 @RestController
 @RequestMapping(value = "/api/bans")
@@ -18,22 +16,5 @@ public class BanGiverRestController {
     @PutMapping("/check")
     public void checkTemporaryBansExpiration() {
         banGiverFacade.checkTemporaryBansExpiration();
-    }
-
-    @PutMapping("/user/{userId}/ban/permanent")
-    public PermanentBanResponse banUserPermanently(@PathVariable String userId,
-                                                   @RequestBody UserPermanentBanRequest userPermanentBanRequest) {
-        return banGiverFacade.banUserPermanently(userId, userPermanentBanRequest);
-    }
-
-    @PutMapping("/user/{userId}/ban/temporary")
-    public TemporaryBanResponse banUserTemporary(@PathVariable String userId,
-                                                 @RequestBody UserTemporaryBanRequest userTemporaryBanRequest) {
-        return banGiverFacade.banUserTemporary(userId, userTemporaryBanRequest);
-    }
-
-    @PutMapping("/user/{userId}/revoke")
-    public void revokeUserBans(@PathVariable String userId) {
-        banGiverFacade.revokeBansByUserId(userId);
     }
 }
--- src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/controller/userdatareceiver/UserDataReceiverRestController.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/controller/userdatareceiver/UserDataReceiverRestController.java
@@ -2,6 +2,11 @@ package pl.jdacewicz.socialmediaserver.infrastructure.controller.userdatareceive
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import pl.jdacewicz.socialmediaserver.bangiver.BanGiverFacade;
+import pl.jdacewicz.socialmediaserver.bangiver.dto.PermanentBanResponse;
+import pl.jdacewicz.socialmediaserver.bangiver.dto.TemporaryBanResponse;
+import pl.jdacewicz.socialmediaserver.bangiver.dto.UserPermanentBanRequest;
+import pl.jdacewicz.socialmediaserver.bangiver.dto.UserTemporaryBanRequest;
 import pl.jdacewicz.socialmediaserver.reportdatareceiver.ReportDataReceiverFacade;
 import pl.jdacewicz.socialmediaserver.reportdatareceiver.dto.ReportRequest;
 
@@ -11,10 +16,29 @@ import pl.jdacewicz.socialmediaserver.reportdatareceiver.dto.ReportRequest;
 public class UserDataReceiverRestController {
 
     private final ReportDataReceiverFacade reportDataReceiverFacade;
+    private final BanGiverFacade banGiverFacade;
 
     @PostMapping("/{id}/report")
     public void reportUser(@PathVariable String id,
                            @RequestBody ReportRequest reportRequest) {
         reportDataReceiverFacade.report(id, reportRequest, "USER");
+    }
+
+
+    @PutMapping("/{id}/ban/permanent")
+    public PermanentBanResponse banUserPermanently(@PathVariable String id,
+                                                   @RequestBody UserPermanentBanRequest userPermanentBanRequest) {
+        return banGiverFacade.banUserPermanently(id, userPermanentBanRequest);
+    }
+
+    @PutMapping("/{id}/ban/temporary")
+    public TemporaryBanResponse banUserTemporary(@PathVariable String id,
+                                                 @RequestBody UserTemporaryBanRequest userTemporaryBanRequest) {
+        return banGiverFacade.banUserTemporary(id, userTemporaryBanRequest);
+    }
+
+    @PutMapping("/{id}/ban/revoke")
+    public void revokeUserBans(@PathVariable String id) {
+        banGiverFacade.revokeBansByUserId(id);
     }
 }
