7613b379720dd3e343ee5a57f0800e783ff7faba
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-11-08 11:44:55 +0100

    Created User,Comment and Post reports

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/reportdatareceiver/PostReport.java
@@ -0,0 +1,17 @@
+package pl.jdacewicz.socialmediaserver.reportdatareceiver;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reports")
+@Getter
+@SuperBuilder
+class PostReport extends Report {
+
+    @Id
+    private String reportId;
+
+    private String postId;
+}
