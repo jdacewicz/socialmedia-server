c990f13d3fd794a29524a3f5af5701086c5512a7
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-11-08 11:44:24 +0100

    Created ReportType

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/reportdatareceiver/ReportType.java
@@ -0,0 +1,12 @@
+package pl.jdacewicz.socialmediaserver.reportdatareceiver;
+
+enum ReportType {
+    NUDITY,
+    SPAM,
+    FAKE_NEWS,
+    TERRORISM,
+    SELF_HARM,
+    PERSECUTE,
+    DRASTIC_CONTENT,
+    ILLEGAL_CONTENT
+}
