ccf24b0c6c5fad7ca00de4a9947bd5cc2086a6bf
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-11-09 15:05:16 +0100

    Written tests of BannedWordService

---
+++ src/test/java/pl/jdacewicz/socialmediaserver/bannedwordschecker/BannedWordRepositoryTest.java
@@ -0,0 +1,134 @@
+package pl.jdacewicz.socialmediaserver.bannedwordschecker;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@SuppressWarnings("all")
+class BannedWordRepositoryTest implements BannedWordRepository {
+
+    List<BannedWord> database = new LinkedList<>();
+
+    @Override
+    public <S extends BannedWord> S save(S entity) {
+        database.add(entity);
+        return entity;
+    }
+
+    @Override
+    public <S extends BannedWord> S insert(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends BannedWord> List<S> insert(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public <S extends BannedWord> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends BannedWord> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends BannedWord> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends BannedWord> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends BannedWord> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends BannedWord> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends BannedWord, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    @Override
+    public <S extends BannedWord> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<BannedWord> findById(String s) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public List<BannedWord> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<BannedWord> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(BannedWord entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends BannedWord> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<BannedWord> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<BannedWord> findAll(Pageable pageable) {
+        return null;
+    }
+}
---
+++ src/test/java/pl/jdacewicz/socialmediaserver/bannedwordschecker/BannedWordServiceTest.java
@@ -0,0 +1,41 @@
+package pl.jdacewicz.socialmediaserver.bannedwordschecker;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.jdacewicz.socialmediaserver.bannedwordschecker.dto.BanWordRequest;
+import pl.jdacewicz.socialmediaserver.userdatareceiver.UserDataReceiverFacade;
+import pl.jdacewicz.socialmediaserver.userdatareceiver.dto.UserDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class BannedWordServiceTest {
+
+    BannedWordService bannedWordService;
+
+    BannedWordRepositoryTest bannedWordRepositoryTest;
+    UserDataReceiverFacade userDataReceiverFacade;
+
+    @BeforeEach
+    void setUp() {
+        userDataReceiverFacade = Mockito.mock(UserDataReceiverFacade.class);
+        bannedWordRepositoryTest = new BannedWordRepositoryTest();
+        bannedWordService = new BannedWordService(bannedWordRepositoryTest, userDataReceiverFacade);
+    }
+
+    @Test
+    void should_return_created_banned_word_when_creating_banned_word() {
+        //Given
+        var banWordRequest = new BanWordRequest("word");
+        var loggedUser = UserDto.builder()
+                .userId("id")
+                .build();
+        when(userDataReceiverFacade.getLoggedInUser()).thenReturn(loggedUser);
+        //When
+        var result = bannedWordService.createBannedWord(banWordRequest);
+        //Then
+        assertEquals(loggedUser.userId(), result.creatorId());
+        assertEquals(banWordRequest.word(), result.word());
+    }
+}
