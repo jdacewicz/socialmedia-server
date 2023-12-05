ab91a340fa66cb7bb0fd777c527cad2a20d0b041
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-12-05 14:15:27 +0100

    Tests of giving ban already banned User prohibition

--- src/test/java/pl/jdacewicz/socialmediaserver/bangiver/BanGiverFacadeTest.java
+++ src/test/java/pl/jdacewicz/socialmediaserver/bangiver/BanGiverFacadeTest.java
@@ -3,12 +3,14 @@ package pl.jdacewicz.socialmediaserver.bangiver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import pl.jdacewicz.socialmediaserver.bangiver.dto.UserPermanentBanRequest;
 import pl.jdacewicz.socialmediaserver.bangiver.dto.UserTemporaryBanRequest;
 import pl.jdacewicz.socialmediaserver.userdatareceiver.UserDataReceiverFacade;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 class BanGiverFacadeTest {
@@ -33,7 +35,7 @@ class BanGiverFacadeTest {
     }
 
     @Test
-    void should_not_throw_exception_when_banning_user_temporary_by_id_of_user_not_permanently_banned() {
+    void should_not_throw_exception_when_banning_user_temporary_by_id_of_user_not_banned() {
         //Given
         var userId = "id";
         var userTemporaryBanRequest = new UserTemporaryBanRequest(LocalDateTime.now(), "reason");
@@ -44,7 +46,7 @@ class BanGiverFacadeTest {
                 .reason(userTemporaryBanRequest.reason())
                 .build();
         var authenticationHeader = "token";
-        when(banGiverService.isPermanentBanAssignedToUser(userId)).thenReturn(false);
+        when(banGiverService.isBanAssignedToUser(userId)).thenReturn(false);
         when(temporaryBanGiverService.createBan(userId, authenticationHeader, userTemporaryBanRequest)).thenReturn(tempBan);
         //When
         //Then
@@ -52,15 +54,46 @@ class BanGiverFacadeTest {
     }
 
     @Test
-    void should_throw_unsupported_operation_exception_when_banning_user_temporary_by_id_of_permanently_banned_user() {
+    void should_throw_unsupported_operation_exception_when_banning_user_temporary_by_id_of_banned_user() {
         //Given
         var userId = "id";
         var userTemporaryBanRequest = new UserTemporaryBanRequest(LocalDateTime.now(), "reason");
         var authenticationHeader = "token";
-        when(banGiverService.isPermanentBanAssignedToUser(userId)).thenReturn(true);
+        when(banGiverService.isBanAssignedToUser(userId)).thenReturn(true);
         //When
         //Then
         assertThrows(UnsupportedOperationException.class,
                 () -> banGiverFacade.banUserTemporary(userId, authenticationHeader, userTemporaryBanRequest));
+    }
+
+    @Test
+    void should_not_throw_exception_when_banning_user_permanently_by_id_of_user_not_banned() {
+        //Given
+        var userId = "id";
+        var userPermanentBanRequest = new UserPermanentBanRequest("reason");
+        var permBan = Ban.builder()
+                .banId("id")
+                .from(LocalDateTime.now())
+                .reason(userPermanentBanRequest.reason())
+                .build();
+        var authenticationHeader = "token";
+        when(banGiverService.isBanAssignedToUser(userId)).thenReturn(false);
+        when(banGiverService.createBan(userId, authenticationHeader, userPermanentBanRequest)).thenReturn(permBan);
+        //When
+        //Then
+        assertDoesNotThrow(() -> banGiverFacade.banUserPermanently(userId, authenticationHeader, userPermanentBanRequest));
+    }
+
+    @Test
+    void should_throw_unsupported_operation_exception_when_banning_user_permanently_by_id_of_banned_user() {
+        //Given
+        var userId = "id";
+        var userPermanentBanRequest = new UserPermanentBanRequest("reason");
+        var authenticationHeader = "token";
+        when(banGiverService.isBanAssignedToUser(userId)).thenReturn(true);
+        //When
+        //Then
+        assertThrows(UnsupportedOperationException.class,
+                () -> banGiverFacade.banUserPermanently(userId, authenticationHeader, userPermanentBanRequest));
     }
 }
