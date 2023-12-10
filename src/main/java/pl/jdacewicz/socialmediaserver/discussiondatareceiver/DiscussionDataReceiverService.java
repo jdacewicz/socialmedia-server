557fe50425d5fdd74cefbb0371c6c265b8ee0658
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-12-10 21:14:08 +0100

    Created DiscussionDataReceiverService interface

--- src/main/java/pl/jdacewicz/socialmediaserver/discussiondatareceiver/DiscussionDataReceiverService.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/discussiondatareceiver/DiscussionDataReceiverService.java
@@ -4,23 +4,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import pl.jdacewicz.socialmediaserver.reactionuser.dto.ReactionUser;
 
-abstract class DiscussionDataReceiverService<T extends Discussion<T>> {
+interface DiscussionDataReceiverService<T> {
 
-    abstract T getDiscussionById(String id);
+    T getDiscussionById(String id);
 
-    abstract Page<T> getDiscussionsByContentContaining(String phrase, Pageable pageable);
+    Page<T> getDiscussionsByContentContaining(String phrase, Pageable pageable);
 
-    abstract T reactToDiscussionById(String id, ReactionUser reactionUser);
+    T reactToDiscussionById(String id, ReactionUser reactionUser);
 
-    abstract void deleteDiscussion(T discussion);
+    void deleteDiscussionById(String id);
 
-
-    abstract void deleteAllDiscussions();
-
-    T react(T discussion, ReactionUser reactionUser) {
-        if (discussion.isReactionUserStored(reactionUser)) {
-            return discussion.withoutReactionUser(reactionUser);
-        }
-        return discussion.withReactionUser(reactionUser);
-    }
+    void deleteAllDiscussions();
 }
