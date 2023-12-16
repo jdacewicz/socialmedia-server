78e408952ce818dc80c87abd7434f58bc707c23b
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-12-16 18:15:07 +0100

    Refactoring DiscussionDataReceiver c.d.

--- src/main/java/pl/jdacewicz/socialmediaserver/discussiondatareceiver/DiscussionMapper.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/discussiondatareceiver/DiscussionMapper.java
@@ -1,10 +1,46 @@
 package pl.jdacewicz.socialmediaserver.discussiondatareceiver;
 
+import com.vdurmont.emoji.EmojiParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.jdacewicz.socialmediaserver.discussiondatareceiver.dto.DiscussionDto;
+import pl.jdacewicz.socialmediaserver.elapsedtimeformatter.ElapsedDateTimeFormatterFacade;
+import pl.jdacewicz.socialmediaserver.filemapper.FileMapperFacade;
+import pl.jdacewicz.socialmediaserver.filemapper.dto.MapRequest;
+import pl.jdacewicz.socialmediaserver.reactioncounter.ReactionCounterFacade;
+
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-interface DiscussionMapper<T extends Discussion<T>, S> {
+@Component
+@RequiredArgsConstructor
+class DiscussionMapper {
 
-    Set<S> mapToDto(Set<T> discussions);
+    private final ReactionCounterFacade reactionCounterFacade;
+    private final ElapsedDateTimeFormatterFacade elapsedDateTimeFormatterFacade;
+    private final FileMapperFacade fileMapperFacade;
 
-    S mapToDto(T discussion);
+    <T extends Discussion<T>> Set<DiscussionDto> mapToDto(Set<T> foundPosts) {
+        return foundPosts.stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toSet());
+    }
+
+    public DiscussionDto mapToDto(Discussion<?> discussion) {
+        var image = fileMapperFacade.mapToFile(new MapRequest(discussion.getImageName(), discussion.getFolderDirectory()));
+        var elapsedDateTime = elapsedDateTimeFormatterFacade.formatDateTime(discussion.getCreationDateTime());
+        var reactionCounts = reactionCounterFacade.countReactions(discussion.getReactionUsers());
+        return DiscussionDto.builder()
+                .postId(discussion.getDiscussionId())
+                .content(EmojiParser.parseToUnicode(discussion.getContent()))
+                .creator(discussion.getCreator())
+                .image(image)
+                .elapsedDateTime(elapsedDateTime)
+                .reactionCounts(reactionCounts)
+                .build();
+    }
+
+    public List<DiscussionDto> mapToDto(List<? extends Post> foundPosts) {
+    }
 }
