5d85041a5f755e6d4347e5bb7cb429dcc1f5ce83
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-12-17 14:22:45 +0100

    Refactored endpoints in DataGrouperRestController

--- src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/controller/datagrouper/DataGrouperRestController.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/controller/datagrouper/DataGrouperRestController.java
@@ -9,15 +9,25 @@ import org.springframework.web.multipart.MultipartFile;
 import pl.jdacewicz.socialmediaserver.datagrouper.DataGrouperFacade;
 import pl.jdacewicz.socialmediaserver.datagrouper.dto.GroupDto;
 import pl.jdacewicz.socialmediaserver.datagrouper.dto.GroupRequest;
+import pl.jdacewicz.socialmediaserver.discussiondatareceiver.DiscussionDataReceiverFacade;
+import pl.jdacewicz.socialmediaserver.discussiondatareceiver.dto.DiscussionDto;
+import pl.jdacewicz.socialmediaserver.discussiondatareceiver.dto.GroupedPostCreationRequest;
+import pl.jdacewicz.socialmediaserver.reportdatareceiver.ReportDataReceiverFacade;
+import pl.jdacewicz.socialmediaserver.reportdatareceiver.dto.ReportRequest;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/groups")
 @RequiredArgsConstructor
 public class DataGrouperRestController {
 
+    final static String discussionType = "GROUPED_POST";
+
     private final DataGrouperFacade dataGrouperFacade;
+    private final DiscussionDataReceiverFacade discussionDataReceiverFacade;
+    private final ReportDataReceiverFacade reportDataReceiverFacade;
 
     @GetMapping("/{id}")
     public GroupDto getGroupById(@PathVariable @NotBlank String id,
@@ -25,12 +35,29 @@ public class DataGrouperRestController {
         return dataGrouperFacade.getGroupById(id, type);
     }
 
-    @GetMapping("/participant/{participantId}")
+    @GetMapping("/posts/{id}")
+    public DiscussionDto getGroupedPostById(@PathVariable @NotBlank String id) {
+        return discussionDataReceiverFacade.getDiscussionById(id, discussionType);
+    }
+
+    @GetMapping("/participants/{participantId}")
     public Page<GroupDto> getGroupsByParticipantId(@PathVariable @NotBlank String participantId,
                                                    @RequestParam @NotBlank String type,
                                                    @RequestParam int pageNumber,
                                                    @RequestParam int pageSize) {
         return dataGrouperFacade.getGroupsByParticipantId(participantId, type, pageNumber, pageSize);
+    }
+
+    @GetMapping("/posts/users/{userId}")
+    public Page<DiscussionDto> getGroupedPostsByUserId(@PathVariable String userId,
+                                                       @RequestParam int pageNumber,
+                                                       @RequestParam int pageSize) {
+        return discussionDataReceiverFacade.getPostsByUserId(userId, discussionType, pageNumber, pageSize);
+    }
+
+    @GetMapping("/posts")
+    public List<DiscussionDto> getRandomGroupedPosts() {
+        return discussionDataReceiverFacade.getRandomPosts(discussionType);
     }
 
     @PostMapping
@@ -41,9 +68,35 @@ public class DataGrouperRestController {
         return dataGrouperFacade.createGroup(groupImage, authorizationHeader, groupRequest, type);
     }
 
+    @PostMapping("/posts")
+    public DiscussionDto createGroupedPost(@RequestHeader("Authorization") String authorizationHeader,
+                                           @RequestPart MultipartFile postImage,
+                                           @RequestPart @Valid GroupedPostCreationRequest groupedPostCreationRequest) throws IOException {
+        return discussionDataReceiverFacade.createDiscussion(discussionType, authorizationHeader, postImage, groupedPostCreationRequest);
+    }
+
+    @PostMapping("/posts/{id}/report")
+    public void reportPost(@PathVariable String id,
+                           @RequestHeader("Authorization") String authorizationHeader,
+                           @RequestBody ReportRequest reportRequest) {
+        reportDataReceiverFacade.report(id, authorizationHeader, reportRequest, discussionType);
+    }
+
+    @PutMapping("/posts/{postId}/react/{reactionId}")
+    public DiscussionDto reactToGroupedPost(@RequestHeader("Authorization") String authorizationHeader,
+                                            @PathVariable @NotBlank String postId,
+                                            @PathVariable @NotBlank String reactionId) {
+        return discussionDataReceiverFacade.reactToDiscussion(reactionId, postId, discussionType, authorizationHeader);
+    }
+
     @DeleteMapping("/{id}")
     public void deleteGroup(@PathVariable @NotBlank String id,
                             @RequestParam @NotBlank String type) throws IOException {
         dataGrouperFacade.deleteGroupById(id, type);
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void deleteGroupedPost(@PathVariable @NotBlank String id) throws IOException {
+        discussionDataReceiverFacade.deleteDiscussion(id, discussionType);
     }
 }
